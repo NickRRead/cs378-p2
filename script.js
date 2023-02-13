e6482620435575776861d27d287d69d36a91d5eb
NickRRead <92062231+NickRRead@users.noreply.github.com>
2023-02-13 15:19:04 -0600

    Added template button functionality

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-alert('Hello world');
+alert('Hit ok please');
+
+var mac_button = document.getElementByID('mac-button');
+var pasta_button = document.getElementByID('pasta-button');
+var taco_button = document.getElementByID('taco-button');
+var ench_button = document.getElementByID('ench-button');
+var order_button = document.getElementByID('order-button');
+var clear_button = document.getElementByID('clear-button');
+
+mac_button.addEventListener('click', mac_func);
+pasta_button.addEventListener('click', pasta_func);
+taco_button.addEventListener('click', taco_func);
+ench_button.addEventListener('click', ench_func);
+
+order_button.addEventListener('click', order_func);
+clear_button.addEventListener('click', clear_func);
+
+function mac_func() {
+  alert('mac_button clicked!');
+}
+
+function pasta_func() {
+  alert('pasta_button clicked!');
+}
+
+function taco_func() {
+  alert('taco_button clicked!');
+}
+
+function ench_func() {
+  alert('ench_button clicked!');
+}
+
+function order_func() {
+  alert('order_button clicked!');
+}
+
+function clear_func() {
+  alert('clear_button clicked!');
+}
